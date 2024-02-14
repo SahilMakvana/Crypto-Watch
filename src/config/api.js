176d096833d114398159f6cef2dc73256bfddef9
SahilMakvana <67103415+SahilMakvana@users.noreply.github.com>
2024-02-14 21:39:10 +0530

    Update api.js

--- src/config/api.js
+++ src/config/api.js
@@ -9,3 +9,4 @@ export const TrendingCoins = (currency) => `https://api.coingecko.com/api/v3/coi
 // Developed by SahilMakvana
 // Developed by SahilMakvana
 // Developed by SahilMakvana
+// Developed by SahilMakvana
